4e2a5cfb6ec044725cf8e8d2469bec89eaca0cde
Muhammad Sabir <sabir.tabi1122@gmail.com>
2024-02-23 17:41:56 +0500

    Create stucture of all of the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <main className="font-bold">Home Page</main>;
-}
+const Home = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="wide:padding-r padding-b xl:p-1">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special Offer</section>
+      <section className="padding bg-pale-blue">Customer Reviews</section>
+      <section className="padding-x w-full py-16 sm:py-32">Subscribe</section>
+      <section className="padding-x padding-t bg-black pb-8">Footer</section>
+    </main>
+  );
+};
+
+export default Home;
