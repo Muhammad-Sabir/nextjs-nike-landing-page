4e51556760f96a81998f48e352eef0ff4749771b
Muhammad Sabir <sabir.tabi1122@gmail.com>
2024-02-25 17:04:27 +0500

    Finalize ServiceCard

---
+++ src/components/ServiceCard.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+
+type ServiceCardProps = {
+  imgURL: string;
+  label: string;
+  subtext: string;
+};
+
+const ServiceCard = ({ imgURL, label, subtext }: ServiceCardProps) => {
+  return (
+    <div className="flex w-full flex-1 flex-col rounded-[20px] px-10 py-16 shadow-3xl sm:w-[350px] sm:min-w-[350px]">
+      <div className="flex size-11 items-center justify-center rounded-full bg-coral-red">
+        <Image src={imgURL} alt={label} width={24} height={24} />
+      </div>
+
+      <h3 className="mt-5 font-palanquin text-3xl font-bold leading-normal">
+        {label}
+      </h3>
+      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">
+        {subtext}
+      </p>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,15 @@
+import { services } from "@/constants";
 import React from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex flex-wrap justify-center gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
